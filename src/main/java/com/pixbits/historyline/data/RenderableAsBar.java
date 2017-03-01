d51a13ec5074e99a78bdd8dda9a93c131052490c
Jack <jack@pixbits.com>
2017-03-01 21:48:49 +0100

    working on unified interface for renderable elements

---
+++ src/main/java/com/pixbits/historyline/data/RenderableAsBar.java
@@ -0,0 +1,9 @@
+package com.pixbits.historyline.data;
+
+import com.pixbits.historyline.data.dates.TimeSpan;
+
+public interface RenderableAsBar
+{
+   TimeSpan getTimeSpan();
+   String getCaption();
+}
--- src/main/java/com/pixbits/historyline/data/people/Person.java
+++ src/main/java/com/pixbits/historyline/data/people/Person.java
@@ -1,8 +1,9 @@
 package com.pixbits.historyline.data.people;
 
+import com.pixbits.historyline.data.RenderableAsBar;
 import com.pixbits.historyline.data.dates.TimeSpan;
 
-public class Person
+public class Person implements RenderableAsBar
 {
   final Event birth;
   final Event death;
@@ -19,4 +20,7 @@ public class Person
   {
     return new TimeSpan(birth.getDate(), death.getDate());
   }
+  
+  @Override public String getCaption() { return name; }
+  @Override public TimeSpan getTimeSpan() { return new TimeSpan(birth.getDate(), death.getDate()); }
 }
